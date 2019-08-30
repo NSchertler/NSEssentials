e5bceb01708368756528fdc95054bd3d05b7b745
Nico Schertler <nschertl@mail.ubc.ca>
2019-08-30 16:42:32 -0700

    use correct types

--- include/nsessentials/math/FastTrigonometry.h
+++ include/nsessentials/math/FastTrigonometry.h
@@ -22,22 +22,22 @@ namespace nse
 		{
 			//formula from https://stackoverflow.com/a/36387954/1210053
 			
-			T xSq = x * x;
+			T xSq = (T)(x * x);
 		#ifdef FP_FAST_FMA
-			T num = x * std::fma(0.9217841528914573, xSq, -0.939115566365855);
-			T denom = 1 + xSq * std::fma(0.295624144969963174, xSq, -1.2845906244690837);
+			T num = x * std::fma((T)0.9217841528914573, xSq, (T)-0.939115566365855);
+			T denom = 1 + xSq * std::fma((T)0.295624144969963174, xSq, (T)-1.2845906244690837);
 		#else
-			T num = x * (-0.939115566365855 + 0.9217841528914573 * xSq);
-			T denom = 1 + xSq * (-1.2845906244690837 + 0.295624144969963174 * xSq);
+			T num = x * ((T)-0.939115566365855 + (T)0.9217841528914573 * xSq);
+			T denom = 1 + xSq * ((T)-1.2845906244690837 + (T)0.295624144969963174 * xSq);
 		#endif	
-			return M_PI_2 + num / denom;
+			return (T)M_PI_2 + num / denom;
 		}
 
 		//Approximates the arcsin function with an absolute error of about 1°.
 		template <typename T>
 		T FastAsin(T x)
 		{
-			return M_PI_2 - FastAcos(x);
+			return (T)M_PI_2 - FastAcos(x);
 		}
 	}
 }
